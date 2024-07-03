70bb3f51268792e8ea0355cefe4c62837ddf662b
ShaariqBBD <shaariq.patel@bbd.co.za>
2024-07-03 23:39:46 +0200

    Stock exchange stuff

--- backend/src/main/java/com/health_insurance/backend/controller/StockExchangeController.java
+++ backend/src/main/java/com/health_insurance/backend/controller/StockExchangeController.java
@@ -5,14 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.health_insurance.backend.repository.StockExchangeRepository;
+import com.health_insurance.backend.service.MakePayment;
 import com.health_insurance.backend.dto.StockExchangeDto;
 import com.health_insurance.backend.model.StockExchange;
 
+import java.math.BigInteger;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
@@ -21,27 +25,70 @@ public class StockExchangeController {
     @Autowired
     private StockExchangeRepository stockExchangeRepository;
 
+    private final MakePayment makePayment;
+
+    @Autowired
+    public StockExchangeController(MakePayment makePayment) {
+        this.makePayment = makePayment;
+    }
+
     @GetMapping("/list-stockinfo")
     public ResponseEntity<StockExchangeDto> getStockExchangeInfo() {
-        List<StockExchange> stockExchanges = stockExchangeRepository.findAll();
-        StockExchangeDto stockExchangeDto = new StockExchangeDto(stockExchanges);
-        return new ResponseEntity<>(stockExchangeDto, HttpStatus.OK);
+        try {
+            List<StockExchange> stockExchanges = stockExchangeRepository.findAll();
+            StockExchangeDto stockExchangeDto = new StockExchangeDto(stockExchanges);
+            return new ResponseEntity<>(stockExchangeDto, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
-    @PostMapping("/register-callback")
-    public void registerBusiness() {
-        System.out.println("Register business");
+    @PostMapping("/stock-register")
+    public void registerBusiness(@RequestBody Map<String, String> body) {
+        try {
+            String idStr = body.get("id");
+            String tradingIdStr = body.get("tradingId");
+    
+            BigInteger id = new BigInteger(idStr);
+            BigInteger tradingId = new BigInteger(tradingIdStr);
+    
+            StockExchange stockExchange = new StockExchange();
+            stockExchange.setBusinessID(id);
+            stockExchange.setTradingID(tradingId);
+    
+            stockExchangeRepository.save(stockExchange);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
     }
 
-    @PostMapping("/sell-stocks")
-    public void sellStocks() {
-        System.out.println("Sell stocks");
+    @PostMapping("/stock-sell")
+    public void sellStocks(@RequestBody Map<String, String> body) {
+        try {
+            System.out.println(body);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
     }
 
-    @PostMapping("/pay-dividends")
-    public void payDividends() {
-        System.out.println("Pay Dividends");
+    @PostMapping("/stock-dividends")
+    public void payDividends(@RequestBody Map<String, String> body) {
+        try {
+            String referenceId = body.get("referenceId");
+            String creditAccount = "stock-exchange";
+            ResponseEntity<String> paymentResponse = makePayment.createTransaction(creditAccount, 100, referenceId, referenceId);
+            if (paymentResponse.getStatusCode() == HttpStatus.OK) {
+                System.out.println("Dividends paid");
+            }
+            else {
+                System.out.println("Dividends could not be paid");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-
 
 }
--- backend/src/main/java/com/health_insurance/backend/service/MakePayment.java
+++ backend/src/main/java/com/health_insurance/backend/service/MakePayment.java
@@ -20,13 +20,15 @@ public class MakePayment {
     private String authToken;
     private String url = "http://api.commercialbank.projects.bbdgrad.com/transactions/create";
 
-    public ResponseEntity<String> createTransaction(double amount, String debitRef, String creditRef) {
+    public ResponseEntity<String> createTransaction(String creditAcc, double amount, String debitRef, String creditRef) {
         RestTemplate restTemplate = new RestTemplate();
 
+        int amountInt = (int) amount;
+
         MakePaymentDto.Transaction transaction = new MakePaymentDto.Transaction();
-        transaction.setDebitAccountName("HealthCare");
-        transaction.setCreditAccountName("HealthInsurance");
-        transaction.setAmount(amount);
+        transaction.setDebitAccountName("health-insurance");
+        transaction.setCreditAccountName(creditAcc);
+        transaction.setAmount(amountInt);
         transaction.setDebitRef(debitRef);
         transaction.setCreditRef(creditRef);
 
@@ -35,7 +37,7 @@ public class MakePayment {
 
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-type", "application/json");
-        // headers.setBearerAuth(authToken);
+        headers.set("X-Origin", "health-insurance");
 
         HttpEntity<MakePaymentDto> requestEntity = new HttpEntity<>(requestBody, headers);
 
