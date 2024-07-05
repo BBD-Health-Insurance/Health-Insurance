5b5a80865eba457f9f4d6dccf039d7d815063647
devonY3303 <159463977+devonY3303@users.noreply.github.com>
2024-07-05 09:20:33 +0200

    feat: Cors fix

---
+++ backend/src/main/java/com/health_insurance/backend/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.health_insurance.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://alex-terraform.d32tpokpqcrt9l.amplifyapp.com");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
